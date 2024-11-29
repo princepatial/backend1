dfcfe050bfe6d740cb533279631e911babf37ce4
Prince Patial <159170980+princepatial@users.noreply.github.com>
2024-11-29 11:18:12 +0530

    Update orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const { checkout, getAllOrders } = require('../Controller/ordercontoller');
+const { checkout, getAllOrders, updateOrder, deleteOrder } = require('../Controller/ordercontoller');
 const router = express.Router();
 
 router.post('/checkout', checkout);
 
 
 router.get('/', getAllOrders);
+
+
+router.put('/:id', orderController.updateOrder);
+
+
+router.delete('/:id', orderController.deleteOrder);
 
 
 module.exports = router;
